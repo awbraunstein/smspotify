689f0c63a7adbac2bf04dd5b72929aca59e9c497
Andrew Braunstein <awbraunstein@gmail.com>
2011-10-05 11:49:01 -0400

    Moved app key and secret to config.rb

--- smspotify.rb
+++ smspotify.rb
@@ -1,15 +1,14 @@
 require 'rubygems'
 require 'twilio-ruby'
 require 'hallon'
-
-# put your own credentials here
-account_sid = 'AC211b4e7f967bbbf12edcf890057b3b62'
-auth_token = '678f0bf63d6bc201b26075a0cb302270'
+require 'sinatra'
+requitre 'config'
 
 # set up a client to talk to the Twilio REST API
-@client = Twilio::REST::Client.new account_sid, auth_token
+@client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
 
 @account = @client.account
 
 @account.sms.messages.create(:from => '+12158746339', :to => '+15165265739', :body => 'Hey there!')
+
 
