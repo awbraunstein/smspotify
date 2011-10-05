ca5dc8e5526f28f1c338082e98b9af2065710f80
Andrew Braunstein <awbraunstein@gmail.com>
2011-10-05 14:50:43 -0400

    added spotify parser and set up sinatra routes

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source :rubygems
 
 gem 'sinatra'
-gem 'twilio-ruby
+gem 'twilio-ruby'
 gem 'hallon'
+gem 'net/http'
+gem 'json'
--- smspotify.rb
+++ smspotify.rb
@@ -1,17 +1,49 @@
 require 'rubygems'
 require 'twilio-ruby'
-#require 'hallon'
+require 'hallon'
 require 'sinatra'
+require 'net/http'
+require 'json'
+
 require './config'
-
-
-ENV['TWILIO_ACCOUNT_SID'] = 'AC211b4e7f967bbbf12edcf890057b3b62'
-ENV['TWILIO_AUTH_TOKEN'] = '678f0bf63d6bc201b26075a0cb302270'
 
 # set up a client to talk to the Twilio REST API
 @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
-
 @account = @client.account
 
-@account.sms.messages.create(:from => '+12158746339', :to => '+15165265739', :body => 'Hey there!')
+get '/' do
+  "welcome to smspotify"
+end
+
+post '/' do
+
+  response = Twilio::TwiML::Response.new do |r|
+    r.Sms 'hello there'
+  end
+  "#{response.text}"
+  puts reponse.text
+end
+
+class GetArtists
+  # @spotify_artist_get_request = "http://ws.spotify.com/search/1/artist.json?q="
+  @spotify_get_request = "http://ws.spotify.com/search/1/track.json?q="
+    
+  def GetArtists.get_spotify_URIs(artists)
+    uri_list = []
+    artists.each do |artist|
+      encoded_artist = artist.gsub(/\s/, '+')
+      request_url = @spotify_get_request+encoded_artist
+      data = Net::HTTP.get_response URI.parse(request_url)
+      json_obj = JSON.parse(data.body)
+      
+      track_uri = json_obj["tracks"][0]["href"]
+      
+      p track_uri
+      
+      uri_list << track_uri
+    end
+    uri_list
+  end  
+  
+end
 
